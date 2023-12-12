3c4c6accb5d8a855e80c2a8fd798ae72b5a24856
Roman Baeriswyl <r.baeriswyl@gmail.com>
2023-12-12 21:37:34 +0100

    Fix startup

--- files/Scripts/startup.sh
+++ files/Scripts/startup.sh
@@ -5,4 +5,4 @@ python3 /prepare.py
 
 # Start the server
 cd /ut-server/System
-./ucc-bin server $UT_SERVERURL ini=UnrealTournament.ini log=ut.log -nohomedir -lanplay
+./ucc-bin-x86 server $UT_SERVERURL ini=UnrealTournament.ini log=ut.log -nohomedir -lanplay
